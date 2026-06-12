fdc06a7487f3aa6f096de0ef25d7562e92c09e27
prem736raj <prem736raj@gmail.com>
2026-06-12 08:52:41 +0530

    Update Color.kt

---
+++ app/src/main/java/com/onelineaday/dailydiary/ui/theme/Color.kt
@@ -0,0 +1,76 @@
+package com.onelineaday.dailydiary.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Palette - Warm Sunset Gradient
+val SunsetAmber = Color(0xFFFFB347)
+val SunsetOrange = Color(0xFFFF8C42)
+val SunsetRose = Color(0xFFFF6B6B)
+val SunsetPink = Color(0xFFEE5A5A)
+
+// Secondary Palette - Soft Lavender
+val LavenderLight = Color(0xFFE8E0FF)
+val LavenderMid = Color(0xFFB8A9C9)
+val LavenderDark = Color(0xFF7B6B8D)
+
+// Accent Colors
+val AccentGold = Color(0xFFFFD700)
+val AccentTeal = Color(0xFF4ECDC4)
+val AccentCoral = Color(0xFFFF6B6B)
+
+// Light Theme Colors
+val LightBackground = Color(0xFFFFFBF5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFF8F4EF)
+val LightOnBackground = Color(0xFF1C1B1F)
+val LightOnSurface = Color(0xFF1C1B1F)
+val LightOnSurfaceVariant = Color(0xFF49454F)
+val LightOutline = Color(0xFFE8E0D8)
+
+// Dark Theme Colors
+val DarkBackground = Color(0xFF0D0D1A)
+val DarkSurface = Color(0xFF1A1A2E)
+val DarkSurfaceVariant = Color(0xFF252542)
+val DarkOnBackground = Color(0xFFF5F5F5)
+val DarkOnSurface = Color(0xFFF5F5F5)
+val DarkOnSurfaceVariant = Color(0xFFCAC4D0)
+val DarkOutline = Color(0xFF3D3D5C)
+
+// Mood Colors
+val MoodHappy = Color(0xFFFFD93D)
+val MoodNeutral = Color(0xFFA8A8A8)
+val MoodSad = Color(0xFF6C9BCF)
+val MoodAngry = Color(0xFFFF6B6B)
+val MoodExcited = Color(0xFFFF9F1C)
+val MoodTired = Color(0xFF9E9E9E)
+val MoodMotivated = Color(0xFF4CAF50)
+val MoodLoved = Color(0xFFE91E63)
+val MoodAnxious = Color(0xFF9C27B0)
+val MoodGrateful = Color(0xFF00BCD4)
+
+// Mood Color Aliases (for EntryDetailScreen compatibility)
+val HappyYellow = MoodHappy
+val NeutralGray = MoodNeutral
+val SadBlue = MoodSad
+val AngryRed = MoodAngry
+val ExcitedPurple = Color(0xFFAA66FF)
+val TiredGray = MoodTired
+val MotivatedOrange = Color(0xFFFF9800)
+val LovedPink = MoodLoved
+val AnxiousTeal = Color(0xFF00BCD4)
+val GratefulGreen = MoodMotivated
+
+// Gradient Colors
+val GradientStart = Color(0xFFFF9A56)
+val GradientMiddle = Color(0xFFFF6B95)
+val GradientEnd = Color(0xFFAA66FF)
+
+// Card Colors
+val CardLight = Color(0xFFFFFFFF)
+val CardDark = Color(0xFF1E1E32)
+
+// Success/Error
+val Success = Color(0xFF4CAF50)
+val Error = Color(0xFFE53935)
+val Warning = Color(0xFFFFA726)
+
